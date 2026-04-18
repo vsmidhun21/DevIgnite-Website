d4c3a0934364a7bc3c7dba6f6cc410a4111ee442
vsmidhun21 <midhun890390@gmail.com>
2026-04-18 11:27:39 +0530

    latest version update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -143,7 +143,7 @@ export default function Hero() {
             <motion.div className="hero__ctas" variants={item}>
               <a
                 id="hero-download-btn"
-                href="https://github.com/vsmidhun21/DevIgnite/releases/download/DevIgnite-v1.0.0/DevIgniteSetup-v1.0.0.exe"
+                href="https://github.com/vsmidhun21/DevIgnite/releases/download/DevIgnite-v1.0.0/DevIgniteSetup-v1.1.0.exe"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn-primary"
